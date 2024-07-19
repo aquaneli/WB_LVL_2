cd2118293e34256e4808a064d639b57643b9877d
Aqua Nelida <aquaneli@si-h4.kzn.21-school.ru>
2024-07-19 14:17:19 +0300

    dev01

--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -6,9 +6,16 @@ import (
 	"github.com/beevik/ntp"
 )
 
-func Test1(t *testing.T) {
+func TestNtp1(t *testing.T) {
 	_, err := ntp.Time("bla.bla")
 	if err == nil {
-		t.Error("Expected error, but got nil")
+		t.Error("Expected error")
+	}
+}
+
+func TestNtp2(t *testing.T) {
+	_, err := ntp.Time("ntp1.ntp-servers.net")
+	if err != nil {
+		t.Error("Expected error")
 	}
 }
