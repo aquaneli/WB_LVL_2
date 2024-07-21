146ee2eff89da50425db35ce900493f4566e0ba4
Aqua Nelida <aquaneli@pr-a4.kzn.21-school.ru>
2024-07-21 22:32:22 +0300

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -19,7 +19,6 @@ func Unpack(arr string) (string, error) {
 	escape := 0
 	/* Проходим в строке по рунам */
 	for i, val := range arr {
-
 		/* Находим ошибку если идут 2 числа подряд, где ни один из них не является escape символом */
 		if val >= '0' && val <= '9' {
 			if i == 0 {
@@ -31,21 +30,17 @@ func Unpack(arr string) (string, error) {
 		} else {
 			check = false
 		}
-
 		/* Проверка escape последовательности */
 		checkEscape(&r, val, &escape, &sb)
-
 		/* Пушим в результирующий String Builder если в стеке есть 2 руны */
 		if len(r) == 2 {
 			sb.WriteString(strings.Repeat(pop(&r), number(&r, &check)))
 		}
 	}
-
 	/* Если остался последний символ то пушим в String Builder */
 	if len(r) == 1 {
 		sb.WriteString(pop(&r))
 	}
-
 	return sb.String(), nil
 }
 
@@ -58,7 +53,6 @@ func checkEscape(r *[]rune, val rune, escape *int, sb *strings.Builder) {
 	} else {
 		*escape = 0
 	}
-
 	if val != '\\' || (*escape)%2 == 0 {
 		push(r, val)
 	}
