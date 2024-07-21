574814a8069dffc53292660447300997430a31ee
Aqua Nelida <aquaneli@pr-a4.kzn.21-school.ru>
2024-07-21 22:46:49 +0300

    dev03

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data := make([]byte, 64)
+	for {
+		_, err := file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(data))
+	}
 }
