3a88fdd4a127195cfaacbb0113a9fe7f91f2b7cf
Aqua Nelida <aquaneli@un-e6.kzn.21-school.ru>
2024-07-22 18:53:35 +0300

    dev03

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,25 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
 
+type flags struct {
+	k int
+	n bool
+	r bool
+	u bool
+}
+
 func main() {
+
+	//Парсинг файла
 	file, err := os.Open("test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	sc := bufio.NewScanner(file)
+	data := make([]string, 0, 5)
+	for sc.Scan() {
+		data = append(data, sc.Text())
+	}
 
-	data := make([]byte, 64)
-	for {
-		_, err := file.Read(data)
-		if err == io.EOF {
-			break
+	//Дефолтная сортировка без флагов
+	// sort.Strings(data)
+
+	//Сортировка с n-й колонки
+	k := 3
+	less := func(i, j int) bool {
+		arr1 := strings.Fields(data[i])
+		arr2 := strings.Fields(data[j])
+		
+		if k-1 >= len(arr1) {
+			return true
 		}
-		fmt.Println(string(data))
+		
+		if k-1 >= len(arr2) {
+			return false
+		}
+
+		return arr1[k-1] < arr2[k-1]
+	}
+
+	//метод сортирует в зависимости хотим ли мы переставлять i и j элементы
+	sort.Slice(data, less)
+
+	for _, val := range data {
+		fmt.Println(val)
 	}
 }
