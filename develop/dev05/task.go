f0483314164c0785d262643a587365e8e92d2d1f
Aqua Nelida <aquaneli@pr-a4.kzn.21-school.ru>
2024-07-27 16:51:14 +0300

    dev05

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -33,7 +33,7 @@ func main() {
 
 	/* ----- */
 
-	reg := "0"
+	reg := "q"
 	compileReg, err := regexp.Compile(reg)
 
 	/* ----- */
@@ -58,57 +58,21 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if args.C > 0 {
+		args.A = args.C
+		args.B = args.C
+	}
+
 	for i, val := range indexBuff {
-		// fmt.Println(val)
 
 		if args.B > 0 {
-
-			if i-1 >= 0 {
-
-				if val-args.B > indexBuff[i-1] {
-					if val-(args.B+1) > indexBuff[i-1] {
-						buffRes = append(buffRes, "--")
-					}
-					buffRes = append(buffRes, buff[val-args.B:val]...)
-				} else {
-					buffRes = append(buffRes, buff[indexBuff[i-1]+1:val]...)
-				}
-
-			} else {
-
-				if args.B > val {
-					buffRes = append(buffRes, buff[:val]...)
-				} else {
-					buffRes = append(buffRes, buff[val-args.B:val]...)
-				}
-
-			}
-
+			buffRes = append(buffRes, BFlag(&buff, &indexBuff, i, args)...)
 		}
 
 		buffRes = append(buffRes, buff[val])
 
 		if args.A > 0 {
-
-			if i+1 < len(indexBuff) {
-
-				if val+args.A+1 < indexBuff[i+1] {
-					buffRes = append(buffRes, buff[val+1:val+args.A+1]...)
-					buffRes = append(buffRes, "--")
-				} else {
-					buffRes = append(buffRes, buff[val+1:indexBuff[i+1]]...)
-				}
-
-			} else {
-
-				if val+args.A+1 < len(buff) {
-					buffRes = append(buffRes, buff[val+1:val+args.A+1]...)
-				} else {
-					buffRes = append(buffRes, buff[val+1:]...)
-				}
-
-			}
-
+			buffRes = append(buffRes, AFlag(&buff, &indexBuff, i, args)...)
 		}
 
 	}
@@ -119,6 +83,60 @@ func main() {
 		fmt.Println(val)
 	}
 
+}
+
+func AFlag(buff *[]string, indexBuff *[]int, i int, args flags) []string {
+	buffRes := []string{}
+	val := (*indexBuff)[i]
+
+	if i+1 < len(*indexBuff) {
+
+		if val+args.A+1 < (*indexBuff)[i+1] {
+			buffRes = append(buffRes, (*buff)[val+1:val+args.A+1]...)
+			buffRes = append(buffRes, "--")
+		} else {
+			buffRes = append(buffRes, (*buff)[val+1:(*indexBuff)[i+1]]...)
+		}
+
+	} else {
+
+		if val+args.A+1 < len(*buff) {
+			buffRes = append(buffRes, (*buff)[val+1:val+args.A+1]...)
+		} else {
+			buffRes = append(buffRes, (*buff)[val+1:]...)
+		}
+
+	}
+
+	return buffRes
+}
+
+func BFlag(buff *[]string, indexBuff *[]int, i int, args flags) []string {
+	buffRes := []string{}
+	val := (*indexBuff)[i]
+
+	if i-1 >= 0 {
+
+		if val-args.B > (*indexBuff)[i-1] {
+			if val-(args.B+1) > (*indexBuff)[i-1] {
+				buffRes = append(buffRes, "--")
+			}
+			buffRes = append(buffRes, (*buff)[val-args.B:val]...)
+		} else {
+			buffRes = append(buffRes, (*buff)[(*indexBuff)[i-1]+1:val]...)
+		}
+
+	} else {
+
+		if args.B > val {
+			buffRes = append(buffRes, (*buff)[:val]...)
+		} else {
+			buffRes = append(buffRes, (*buff)[val-args.B:val]...)
+		}
+
+	}
+
+	return buffRes
 }
 
 func parseFlags() flags {
@@ -132,9 +150,9 @@ func parseFlags() flags {
 	nFlag := flag.Bool("n", false, "сортировать по числовому значению с учетом суффиксов")
 	flag.Parse()
 
-	// if flag.NFlag() > 1 {
-	// 	log.Fatalln("Only 1 flag can be used")
-	// }
+	if flag.NFlag() > 1 {
+		log.Fatalln("Only 1 flag can be used")
+	}
 
 	return flags{
 		A: *AFlag,
