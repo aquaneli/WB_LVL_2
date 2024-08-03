3b0f56aa2be4b32d1245f4dfa0e4c0f6c448b68c
Aqua Nelida <aquaneli@pr-b3.kzn.21-school.ru>
2024-08-03 16:56:21 +0300

    dev09

--- develop/dev09/go.mod
+++ develop/dev09/go.mod
@@ -1,3 +1,5 @@
 module dev09
 
 go 1.21.1
+
+require golang.org/x/net v0.27.0
--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,6 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
 func main() {
+	baseUrl := "https://www.google.com/"
+
+	//подключаемся к серверу
+	client := http.Client{}
+	resp, err := client.Get(baseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	//создаем папку куда будем сохранять файлы
+	err = os.Mkdir("site", 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//считываемвесь html файл или изображения и сохраняем их
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(string(data))
+	os.WriteFile("site/index.html", data, 0777)
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//нужно проходиться по всем тегам рекурсивно и выводить из каждого тега внутренние теги
+	for itr := doc.FirstChild; itr != nil; itr = itr.NextSibling {
+		fmt.Println(itr.Type)
+		if itr.Type == html.ElementNode {
+			fmt.Println(itr.Attr)
+		}
+	}
 
 }
-
