421200f201fac02a94ffa695634bab0d52797ae2
Aqua Nelida <aquaneli@pr-a6.kzn.21-school.ru>
2024-08-11 21:34:56 +0300

    dev11

--- develop/dev11/main.go
+++ develop/dev11/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
+
+type Events struct {
+	UserId int       `json:"user_id"`
+	Date   time.Time `json:"date"`
+}
+
+func HandlerEventsForDay(w http.ResponseWriter, r *http.Request) {
+	// if r.Method == "GET"{
+
+	// }
+	w.Write([]byte("Good job"))
+}
+
+func HandlerCreateEvent(w http.ResponseWriter, r *http.Request) {
+	// if r.Method == "POST"
+}
+
 func main() {
 
+	http.HandleFunc("/create_event", HandlerCreateEvent)
+
+	http.HandleFunc("/events_for_day", HandlerEventsForDay)
+
+	http.ListenAndServe(":8080", nil)
 }
