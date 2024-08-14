1dff057c9f244a9673b19d98f428c7dc4f1aee5c
Aqua Nelida <aquaneli@va-f4.kzn.21-school.ru>
2024-08-14 18:28:13 +0300

    dev11

--- develop/dev11/internal/storage/storage.go
+++ develop/dev11/internal/storage/storage.go
@@ -22,13 +22,26 @@ func NewStorge() *Storage {
 	return &Storage{Items: make(map[int]map[string]events)}
 }
 
-func (s *Storage) AddEvent(UserIdKey, DateVal string) error {
+func getUniqCode() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+
+	return uuid
+}
+
+func (s *Storage) Add(UserIdKey, DateVal string) error {
 	id, err := strconv.Atoi(UserIdKey)
 	if err != nil {
 		return errors.New("некорректный id, введите целое число")
 	}
 
-	dateParse, err := time.Parse("2010-10-25", DateVal)
+	dateParse, err := time.Parse("2006-01-02", DateVal)
 	if err != nil {
 		return errors.New("некорректная дата, введите в формате yyyy-mm-dd")
 	}
@@ -49,26 +62,13 @@ func (s *Storage) AddEvent(UserIdKey, DateVal string) error {
 
 }
 
-func getUniqCode() string {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
-		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-
-	return uuid
-}
-
-func (s *Storage) UpdateEvent(UserIdKey, DateValReplace, CodeEvent string) error {
+func (s *Storage) Update(UserIdKey, DateValReplace, CodeEvent string) error {
 	id, err := strconv.Atoi(UserIdKey)
 	if err != nil {
 		return errors.New("некорректный id, введите целое число")
 	}
 
-	dateParse, err := time.Parse("2010-10-25", DateValReplace)
+	dateParse, err := time.Parse("2006-01-02", DateValReplace)
 	if err != nil {
 		return errors.New("некорректная дата, введите в формате yyyy-mm-dd")
 	}
@@ -91,35 +91,122 @@ func (s *Storage) UpdateEvent(UserIdKey, DateValReplace, CodeEvent string) error
 	}
 
 	if !ok {
-		return errors.New("такого евента не существует")
+		return errors.New("такого эвента не существует")
 	}
 
 	return nil
 }
 
-// func (m *Storage) DeleteEvent(UserIdKey, DateVal string) {
-// 	_, err := checkingIdAndData(UserIdKey, DateVal, "2010-10-10")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	if _, ok := (*m).Items[UserIdKey]; !ok {
-// 		log.Fatal(errors.New("такого пользователя не существует"))
-// 	}
+func (s *Storage) Remove(UserIdKey, DateVal, CodeEvent string) error {
+	id, err := strconv.Atoi(UserIdKey)
+	if err != nil {
+		return errors.New("некорректный id, введите целое число")
+	}
 
-// 	if _, ok := (*m).Items[UserIdKey][DateVal]; !ok {
-// 		log.Fatal(errors.New("нельзя удалить эту дату, потому что она не добавлена"))
-// 	}
-// 	delete((*m).Items[UserIdKey], DateVal)
-// }
+	_, err = time.Parse("2006-01-02", DateVal)
+	if err != nil {
+		return errors.New("некорректная дата, введите в формате yyyy-mm-dd")
+	}
 
-// func (m *Storage) GetEventsForDay(UserIdKey string, Day string) {
-// 	_, err := checkingIdAndData(UserIdKey, Day, "10")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	if _, ok := (*s).Items[id]; !ok {
+		return errors.New("пользователя не существует")
+	}
 
-// 	if _, ok := (*m).Items[UserIdKey]; !ok {
-// 		log.Fatal(errors.New("такого пользователя не существует"))
-// 	}
+	if _, ok := s.Items[id][DateVal]; !ok {
+		return errors.New("на эту дату эвентов нет")
+	}
 
-// }
+	if _, ok := s.Items[id][DateVal].uniqCodeEvents[CodeEvent]; !ok {
+		return errors.New("такого эвента не существет")
+	}
+
+	delete(s.Items[id][DateVal].uniqCodeEvents, CodeEvent)
+
+	return nil
+}
+
+func (s *Storage) GetEventsForDay(UserIdKey string, DateVal string) (*events, error) {
+	id, err := strconv.Atoi(UserIdKey)
+	if err != nil {
+		return nil, errors.New("некорректный id, введите целое число")
+	}
+
+	_, err = time.Parse("2006-01-02", DateVal)
+	if err != nil {
+		return nil, errors.New("некорректная дата, введите в формате yyyy-mm-dd")
+	}
+
+	if _, ok := (*s).Items[id]; !ok {
+		return nil, errors.New("такого пользователя не существует")
+	}
+
+	if _, ok := (*s).Items[id][DateVal]; !ok {
+		return nil, errors.New("на этот день эвентов нет")
+	}
+
+	events := (*s).Items[id][DateVal]
+
+	return &events, nil
+}
+
+func (s *Storage) GetEventsForWeek(UserIdKey string, DateVal string) (*[]events, error) {
+	id, err := strconv.Atoi(UserIdKey)
+	if err != nil {
+		return nil, errors.New("некорректный id, введите целое число")
+	}
+
+	startDate, err := time.Parse("2006-01-02", DateVal)
+	if err != nil {
+		return nil, errors.New("некорректная дата, введите в формате yyyy-mm-dd")
+	}
+
+	if _, ok := (*s).Items[id]; !ok {
+		return nil, errors.New("такого пользователя не существует")
+	}
+
+	eventsForWeek := []events{}
+	for dateStr, event := range s.Items[id] {
+		date, err := time.Parse("2006-01-02", dateStr)
+		if err == nil && date.After(startDate) && date.Before(startDate.AddDate(0, 0, 7)) {
+			eventsForWeek = append(eventsForWeek, event)
+		}
+	}
+
+	if len(eventsForWeek) == 0 {
+		return nil, errors.New("на этой неделе событий нет")
+	}
+
+	return &eventsForWeek, nil
+}
+
+func (s *Storage) GetEventsForYear(UserIdKey string, DateVal string) (*[]events, error) {
+	id, err := strconv.Atoi(UserIdKey)
+	if err != nil {
+		return nil, errors.New("некорректный id, введите целое число")
+	}
+
+	startDate, err := time.Parse("2006-01-02", DateVal)
+	if err != nil {
+		return nil, errors.New("некорректная дата, введите в формате yyyy-mm-dd")
+	}
+
+	if _, ok := (*s).Items[id]; !ok {
+		return nil, errors.New("такого пользователя не существует")
+	}
+
+	eventsForYear := []events{}
+	for dateStr, event := range s.Items[id] {
+		// Парсим дату и проверяем год
+		date, err := time.Parse("2006-01-02", dateStr)
+		if err == nil && date.Year() == startDate.Year() {
+			eventsForYear = append(eventsForYear, event)
+		}
+	}
+
+	// Если нет событий за этот год
+	if len(eventsForYear) == 0 {
+		return nil, errors.New("за этот год нет событий")
+	}
+
+	return &eventsForYear, nil
+}
--- develop/dev11/main.go
+++ develop/dev11/main.go
@@ -1,71 +1,79 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+	storage "dev11/internal/storage"
+	"fmt"
 	"net/http"
 	"time"
 )
 
-type Events struct {
-	UserId int       `json:"user_id"`
-	Date   time.Time `json:"date"`
+// type Events struct {
+// 	UserId int       `json:"user_id"`
+// 	Date   time.Time `json:"date"`
+// }
+
+type HandlerApi struct {
+	calendar storage.Storage
 }
 
 func main() {
+	// http.HandleFunc("/create_event", )
 
-	http.HandleFunc("/create_event", HandlerCreateEvent)
+	// http.HandleFunc("/events_for_day", HandlerEventsForDay)
 
-	http.HandleFunc("/events_for_day", HandlerEventsForDay)
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+	date, _ := time.Parse("2006-01-02", "2015-12-25")
+	fmt.Println(date.ISOWeek())
 
-func HandlerEventsForDay(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	// fmt.Println(r.URL.Scheme, "|", r.URL.String())
-	// fmt.Println(r.URL.Query(), r.Header)
 }
 
 type RequestErr struct {
 	Code string `json:"error"`
 }
 
-func HandlerCreateEvent(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		resMarshal, err := json.Marshal(RequestErr{Code: "HTTP 503"})
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.Write([]byte(resMarshal))
-	} else {
-		//парсинг GET запросов
+func (h *HandlerApi) HandlerCreateEvent(w http.ResponseWriter, r *http.Request) {
+	// if r.Method != http.MethodPost {
+	// 	resMarshal, err := json.Marshal(RequestErr{Code: "HTTP 503"})
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	w.Write([]byte(resMarshal))
+	// } else {
 
-		// for key, val := range r.URL.Query() {
-		// 	fmt.Println(key, val)
-		// }
+	//парсинг GET запросов
+	// for key, val := range r.URL.Query() {
+	// 	fmt.Println(key, val)
+	// }
 
-		//содержимое тела запроса в виде строки
-		// ns := bufio.NewScanner(r.Body)
-		// defer r.Body.Close()
-		// for ns.Scan() {
-		// 	fmt.Println(ns.Text())
-		// }
-		// fmt.Println("finish")
+	//содержимое тела запроса в виде строки
+	// ns := bufio.NewScanner(r.Body)
+	// defer r.Body.Close()
+	// for ns.Scan() {
+	// 	fmt.Println(ns.Text())
+	// }
+	// fmt.Println("finish")
 
-		//парсинг POST запроса
-		// r.ParseForm()
-		// for key, val := range r.Form {
-
-		// }
-
-		// for k, v := range c.Items(){
-		// 	fmt.Println(k, v.Object)
-		// }
+	//парсинг POST запроса
+	r.ParseForm()
+	for key, val := range r.Form {
+		
 
 	}
 
+	// for k, v := range c.Items(){
+	// 	fmt.Println(k, v.Object)
+	// }
+
+	// }
+	// return nil
+}
+
+func (h *HandlerApi) HandlerEventsForDay(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	// fmt.Println(r.URL.Scheme, "|", r.URL.String())
+	// fmt.Println(r.URL.Query(), r.Header)
 }
